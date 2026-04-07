690fdabc1206c1ab05454830ec8105ebec4a5aa1
Guillaume Van Thorre <guillaume.vanthorre@gmail.com>
2026-04-07 15:49:44 +0200

    [ADDED] config - axios, prettier, eslint + global snackbar notifications

--- src/web/eslint.config.js
+++ src/web/eslint.config.js
@@ -1,23 +1,46 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import prettier from "eslint-plugin-prettier";
+import tseslint from "typescript-eslint";
 
-export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+export default [
+    {
+        ignores: ["dist", "node_modules"],
     },
-  },
-])
+
+    js.configs.recommended,
+
+    ...tseslint.configs.recommended,
+
+    {
+        files: ["**/*.{ts,tsx}"],
+        languageOptions: {
+            globals: globals.browser,
+        },
+        plugins: {
+            react,
+            "react-hooks": reactHooks,
+            "react-refresh": reactRefresh,
+            prettier,
+        },
+        rules: {
+            ...react.configs.recommended.rules,
+            ...reactHooks.configs.recommended.rules,
+
+            "react/react-in-jsx-scope": "off",
+            "react-refresh/only-export-components": [
+                "warn",
+                { allowConstantExport: true },
+            ],
+            "prettier/prettier": "warn",
+        },
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+    },
+];
